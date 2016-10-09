05572c08a2c4324e5103abb39019e3d6d975457a
Rainer Stropek <rainer@software-architects.at>
2016-10-09 09:36:41 +0200

    Add JavaScript lab

---
+++ javascript/labs/010-plain/index.js
@@ -0,0 +1,101 @@
+"use strict";
+/*jslint browser: true*/
+/*jslint plusplus: true */
+
+// Run our script once the browser has finished loading all resources
+// and has created the DOM. For details see 
+// https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onload
+window.onload = function () {
+    // Get relevant elements in DOM by id.
+    // Want to know which browsers support getElementById? Check
+    // http://caniuse.com/#search=getElementbyid
+    var list = document.getElementById('pokemonList'),
+        prev = document.getElementById('prev'),
+        next = document.getElementById('next'),
+        loadingIndicator = document.getElementById('loadingIndicator'),
+        currentPage = 1,
+        pageSize = 5,
+        li,
+        content,
+        icon,
+        text,
+        url;
+
+    // Helper function to load Pokemons
+    function loadPokemons() {
+        // Display loading indicator and hide navigation buttons
+        next.hidden = prev.hidden = true;
+        loadingIndicator.hidden = false;
+
+        // Remove all Pokemons from DOM
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+
+        // Build XHR. Need details about XMLHttpRequest? Check
+        // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", function () {
+            // Hide loading indicator
+            loadingIndicator.hidden = true;
+
+            // Parse result
+            var jsonResult = JSON.parse(xhr.response);
+
+            // Selectively show/hide buttons. Note the use of '!!!' here
+            // 
+            prev.hidden = !jsonResult.previous;
+            next.hidden = !jsonResult.next;
+
+            // Iterate over result and create Pokemon results in DOM
+            jsonResult.results.forEach(function (pokemon) {
+                // Build DOM using JavaScript functions. Note that you could
+                // use 'innerHtml' instead.
+                li = document.createElement('div');
+                li.className = 'mdl-list__item';
+
+                content = document.createElement('span');
+                content.className = 'mdl-list__item-primary-content';
+
+                icon = document.createElement('i');
+                icon.className = 'material-icons mdl-list__item-avatar';
+                icon.textContent = 'person';
+
+                text = document.createElement('span');
+                text.textContent = pokemon.name;
+
+                content.appendChild(icon);
+                content.appendChild(text);
+                li.appendChild(content);
+                list.appendChild(li);
+            });
+        });
+
+        // Build URL
+        url = 'http://pokeapi.co/api/v2/pokemon/?limit=' + pageSize;
+        if (currentPage > 1) {
+            url += "&offset=" + (currentPage - 1) * pageSize;
+        }
+
+        // Send request
+        xhr.open('GET', url);
+        xhr.send();
+    }
+
+    // Next page
+    next.onclick = function () {
+        ++currentPage;
+        loadPokemons();
+    };
+
+    // Previous page
+    prev.onclick = function () {
+        if (currentPage > 1) {
+            --currentPage;
+            loadPokemons();
+        }
+    };
+
+    // Initial load of first Pokemon page
+    loadPokemons();
+};
---
+++ javascript/labs/010-plain/readme.md
@@ -0,0 +1,57 @@
+# JavaScript Introduction
+
+
+## Your Job
+
+Create a simple website showing a list of Pokemons.
+
+You have to get the Pokemon list from the RESTful Web API offered by [Pokeapi](http://pokeapi.co/)
+(details see below).
+
+The site must show five Pokemons per page and offer paging buttons (next/previous). The paging
+buttons must be invisible if no more data is available (i.e. hide *previous* button on first page).
+
+Display a loading indicator while loading data from the Web API.
+
+
+## The Web API
+
+The Web API relevant for our sample is documented [here](http://pokeapi.co/docsv2/#pokemon).
+As you can see, you can get a list of five Pokemons using the URL
+[http://pokeapi.co/api/v2/pokemon/?limit=5](http://pokeapi.co/api/v2/pokemon/?limit=5).
+
+Note that the Web API supports paging out of the box. [Here](http://pokeapi.co/docsv2/#resource-lists)
+you can find more details about paging in the Pokemon Web API.
+
+**Tip**: Before you start coding, use [Postman](https://www.getpostman.com/) to try the Web API.
+
+
+## Advices and Tips
+
+* Don't use any JavaScript libraries (e.g. *JQuery*, *Angular*) for this first sample.
+
+* Use a CSS framework like [Bootstrap](http://getbootstrap.com/)
+  [Material Design Lite](https://getmdl.io/styles/index.html) to make your web app look nice.
+  If you are completely new to HTML and CSS, you can skip this and focus on JavaScript.
+
+* You are completely new to HTML, CSS and JavaScript? Here are some important functions and classes
+  you might need in your code:
+  * [getElementById](https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById)
+  * [window.onload](https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onload)
+  * [XMLHttpRequest](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest)
+  * [hidden](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/hidden)-Attribute
+  * [document.createElement](https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement)
+  * [appendChild](https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild)
+  * [className](https://developer.mozilla.org/en-US/docs/Web/API/Element/className)
+  * [textContent](https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent)
+  * [onclick](https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onclick)
+
+## Tools
+
+* Use your favorite editor to write the code. If you are not sure, try 
+  [Visual Studio Code](http://code.visualstudio.com/)
+
+* Run your web app locally using [lite-server](https://github.com/johnpapa/lite-server)
+
+* Check your JavaScript's quality using [JSHint](http://jshint.com/) and
+  [JSLint](http://www.jslint.com/).
