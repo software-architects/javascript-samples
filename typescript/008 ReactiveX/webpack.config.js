5d917cb681fe9f16a519cecaf87f8d6f13cee74f
Rainer Stropek <rainer@software-architects.at>
2018-05-01 09:34:43 +0200

    Update RxJS sample

--- typescript/008 ReactiveX/webpack.config.js
+++ typescript/008 ReactiveX/webpack.config.js
@@ -1,13 +1,15 @@
 module.exports = {
+  mode: "development",
   entry: './index.ts',
   output: {
+    path: '/dist',
     filename: 'bundle.js'
   },
   resolve: {
     extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
   module: {
-    loaders: [
+    rules: [
       { test: /\.tsx?$/, loader: 'ts-loader' }
     ]
   }
